14425bd0c98e29a38e3973485a3ed63da7354c05
Leandro <leandro.n.aguilera@gmail.com>
2022-01-06 11:14:46 -0300

    Centralizar peticiones HTTP

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
+import { obtenerChiste } from "./js/http-provider";
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-// fetch( jokeUrl ).then( resp => {
-    
-//     resp.json().then( data => {
-//         console.log(data.id);
-//         console.log(data.value)
-//     })
-
-// });
-
-fetch( jokeUrl )
-    .then( resp => resp.json() )
-    .then( ({id, value}) => {
-        console.log(id, value);
-    });
+obtenerChiste().then(console.log)
---
+++ src/js/http-provider.js
@@ -0,0 +1,24 @@
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+const obtenerChiste = async () => {
+
+    try {
+
+        const resp = await fetch(jokeUrl);
+
+        if (!resp.ok) return alert('Nose puede realizar la petición');
+
+        const { icon_url, id, value } =  await resp.json();
+
+        return { icon_url, id, value };
+
+    } catch (error) {
+
+        throw error
+    }
+}
+
+export {
+    obtenerChiste
+}
