69e87a326959ecf3bf76f7a134746f8b2e9fc771
Leandro <leandro.n.aguilera@gmail.com>
2022-01-06 19:49:47 -0300

    Crear tabla con usuarios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { obtenerUsuarios } from "./js/http-provider";
-// import { init } from "./js/chistes-pages";
+import { init } from "./js/usuarios-page";
 
-// init()
 
-obtenerUsuarios().then( console.log );
+obtenerUsuarios();
+init();
