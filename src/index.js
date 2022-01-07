3bc6a7c9d105b7a4ce59df2a4488cce0e3611d27
Leandro <leandro.n.aguilera@gmail.com>
2022-01-07 17:32:23 -0300

    Leer usuario

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { obtenerUsuarios } from "./js/http-provider";
-import { init } from "./js/usuarios-page";
+// import { obtenerUsuarios } from "./js/http-provider";
+// import { init } from "./js/usuarios-page";
+import * as CRUD from './js/crud-provider';
 
 
-obtenerUsuarios();
-init();
+// obtenerUsuarios();
+// init();
+
+CRUD.getUsuario( 1 ).then( console.log );
