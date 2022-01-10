8f08b3fc8d7de1dbc6f3262c36c525478b142323
Leandro <leandro.n.aguilera@gmail.com>
2022-01-10 10:00:26 -0300

    Subir archivos

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 // import { obtenerUsuarios } from "./js/http-provider";
 // import { init } from "./js/usuarios-page";
-import * as CRUD from './js/crud-provider';
+import { init } from "./js/archivos-page";
 
+
+init();
 
 // obtenerUsuarios();
-// init();
 
-CRUD.getUsuario( 1 ).then(  );
+// CRUD.getUsuario( 1 ).then(  );
 
-CRUD.crearUsuario({
-    name: 'Leandro',
-    job: 'Devs'
-}).then(  );
+// CRUD.crearUsuario({
+//     name: 'Leandro',
+//     job: 'Devs'
+// }).then(  );
 
-CRUD.actualizarUsuario( 2, {
-    name: 'José',
-    job: 'Tachero'
-}).then( console.log )
+// CRUD.actualizarUsuario( 2, {
+//     name: 'José',
+//     job: 'Tachero'
+// }).then( console.log )
 
-CRUD.borrarUsuario( 1 ).then( console.log )
+// CRUD.borrarUsuario( 1 ).then( console.log )
--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -2,6 +2,10 @@
 const jokeUrl     = 'https://api.chucknorris.io/jokes/random';
 const urlUsuarios = 'https://reqres.in/api/users?page=2';
 
+// Cloudinary
+const cloudPreset = 'rrmcyq7e';
+const cloudUrl    = 'https://api.cloudinary.com/v1_1/dvltn5hcu/upload';
+
 const obtenerChiste = async () => {
 
     try {
@@ -28,7 +32,38 @@ const obtenerUsuarios = async() => {
     return usuarios;
 }
 
+// archivoSubir: File
+const subirImagen = async( archivoSubir ) => {
+
+    const formData = new FormData(); // Crea el resultado de un formulario HTML, listo para hacer posteos
+    formData.append( 'upload_preset', cloudPreset );
+    formData.append( 'file', archivoSubir );
+    
+    try {
+        
+        const resp = await fetch( cloudUrl, {
+            method: 'POST',
+            body: formData
+        });
+
+        if( resp.ok ) {
+            const cloudResp = await resp.json();
+            return cloudResp.secure_url;
+
+        } else {
+            throw await resp.json();
+        }
+
+    } catch (error) {
+
+        throw error;
+        
+    }
+
+}
+
 export {
     obtenerChiste,
-    obtenerUsuarios
+    obtenerUsuarios,
+    subirImagen
 }
